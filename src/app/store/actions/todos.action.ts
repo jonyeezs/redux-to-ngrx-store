b361e6400a15d017c990c96ded7693a7689ed0ca
jonyeezs <jonyeezs@gmail.com>
2018-01-18 00:15:17 +1000

    ✨ Init Store and create Action constants

---
+++ src/app/store/actions/todos.action.ts
@@ -0,0 +1,28 @@
+import { Action } from '@ngrx/store';
+
+import { Todo } from '../../models/Todo';
+// How to write a good action constants
+//   v-- the namespace    v-- this is the benefit of a human, so verbosity is fine
+// [State involve] The action explicitly
+export const LOAD_TODOS = '[Todos] Load Todos';
+export const LOAD_TODOS_FAIL = '[Todos] Fail to Load Todos';
+export const LOAD_TODOS_SUCCESS = '[Todos] Successfully Load Todos';
+
+// Don't need to remember the type
+// Instantiating the class, gives you the information needed to create a valid object.
+export class LoadTodo implements Action {
+  readonly type = LOAD_TODOS;
+}
+
+export class LoadTodoFail implements Action {
+  readonly type = LOAD_TODOS_FAIL;
+  constructor(public payload: any) {}
+}
+
+export class LoadTodoSuccess implements Action {
+  readonly type = LOAD_TODOS_SUCCESS;
+  constructor(public payload: Todo[]) {}
+}
+
+// action types. Will be used for reducers DI
+export type TodosAction = LoadTodo | LoadTodoFail | LoadTodoSuccess;
---
+++ src/app/store/index.ts
@@ -0,0 +1,5 @@
+// This was highly recommended by Todd
+// Why? most probably a single import that can be used
+// in all situations
+export * from './reducers';
+export * from './actions';
