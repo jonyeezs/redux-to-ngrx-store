1c24006d856b24859550034df334c22e389ebbc5
jonyeezs <jonyeezs@gmail.com>
2018-02-01 22:59:23 +1000

    🔊 See how The global state looks like

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(store: Store<any>) {
+    // See what the global store holds
+    store.select(globalStore => globalStore).subscribe((globalStore) => {
+        const today = new Date();
+        const hrs = today.getHours() > 12 ? today.getHours() - 12 : today.getHours();
+        const time = `${hrs}:${today.getMinutes()}:${today.getSeconds()}.${today.getMilliseconds()}`;
+        console.log(time, globalStore);
+    });
+  }
 }
